caf319b56ff6f2805b72993d4fb9130b73b4481d
stone <naefaeron@gmail.com>
2021-11-27 15:20:09 +0300

    Delete allure dependency

--- src/test/java/io/github/eroshenkoam/GithubIssueTest.java
+++ src/test/java/io/github/eroshenkoam/GithubIssueTest.java
@@ -24,7 +24,7 @@ public class GithubIssueTest {
 
     @BeforeEach
     public void initDriver() throws IOException {
-        final String url = "http://localhost:4444/wd/hub";
+        final String url = "http://172.17.0.3:4444/wd/hub";
         WebDriver driver = new RemoteWebDriver(new URL(url), DesiredCapabilities.chrome());
         driver.manage().window().setSize(new Dimension(1920,1024));
         WebDriverRunner.setWebDriver(driver);
